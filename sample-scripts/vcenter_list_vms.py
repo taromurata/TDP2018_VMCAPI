01760b5e76c0fa88752d857bf1f9dc99cc5b1dc6
Taro Murata <taro.murata.1992@gmail.com>
2018-06-28 00:22:52 -0700

    Add sample-scripts dir.

---
+++ sample-scripts/vcenter_list_vms.py
@@ -0,0 +1,23 @@
+#
+# https://github.com/vmware/vsphere-automation-sdk-python
+#
+
+import requests
+import urllib3
+from vmware.vapi.vsphere.client import create_vsphere_client
+session = requests.session()
+
+# Disable cert verification for demo purpose.
+# This is not recommended in a production environment.
+session.verify = False
+
+# Disable the secure connection warning for demo purpose.
+# This is not recommended in a production environment.
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Connect to a vCenter Server using username and password
+vsphere_client = create_vsphere_client(server='<vc_ip>', username='<vc_username>', password='<vc_password>', session=session)
+
+# List all VMs inside the vCenter Server
+vsphere_client.vcenter.VM.list()
+
