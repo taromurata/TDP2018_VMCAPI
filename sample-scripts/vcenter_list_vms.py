160d8522259c707d78c2a209e0bda16bb5f3b57e
Taro Murata <taro.murata.1992@gmail.com>
2018-07-02 17:53:57 -0700

    Modify vcenter_list_vms.py to use info file.

--- sample-scripts/vcenter_list_vms.py
+++ sample-scripts/vcenter_list_vms.py
@@ -2,9 +2,28 @@
 # https://github.com/vmware/vsphere-automation-sdk-python
 #
 
+import argparse
+import yaml
 import requests
 import urllib3
 from vmware.vapi.vsphere.client import create_vsphere_client
+
+# Read an info file.
+parser = argparse.ArgumentParser()
+parser.add_argument('-f', '--file', action='store', type=str)
+
+args = parser.parse_args()
+
+info = yaml.load(open(args.file, "r+"))
+vcenter_ip = info['vcenter']['ip']
+vcenter_username = info['vcenter']['username']
+vcenter_password = info['vcenter']['password']
+
+# print(vcenter_ip)
+# print(vcenter_username)
+# print(vcenter_password)
+
+#
 session = requests.session()
 
 # Disable cert verification for demo purpose.
@@ -16,8 +35,9 @@ session.verify = False
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 # Connect to a vCenter Server using username and password
-vsphere_client = create_vsphere_client(server='<vc_ip>', username='<vc_username>', password='<vc_password>', session=session)
+vsphere_client = create_vsphere_client(server=vcenter_ip,
+        username=vcenter_username, password=vcenter_password, session=session)
 
 # List all VMs inside the vCenter Server
-vsphere_client.vcenter.VM.list()
+print(vsphere_client.vcenter.VM.list())
 
