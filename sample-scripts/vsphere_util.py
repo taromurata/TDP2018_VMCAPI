49700c36d9653688f6b6a8874d0f3c60ac2a3ba6
Taro Murata <taro.murata.1992@gmail.com>
2018-07-18 22:54:07 +0900

    Add create/delete default vm func

--- sample-scripts/vsphere_util.py
+++ sample-scripts/vsphere_util.py
@@ -20,6 +20,14 @@ from tabulate import tabulate
 #from vmware.vapi.vmc.client import create_vmc_client
 from vmware.vapi.vsphere.client import create_vsphere_client
 
+from samples.vsphere.common import sample_cli
+from samples.vsphere.common import sample_util
+from samples.vsphere.common.ssl_helper import get_unverified_session
+from samples.vsphere.common.sample_util import pp
+from samples.vsphere.vcenter.helper import vm_placement_helper
+from samples.vsphere.vcenter.helper.vm_helper import get_vm
+from samples.vsphere.vcenter.setup import testbed
+
 class vSphereUtil():
     def __init__(self):
         self.set_default_vm_spec()
@@ -28,6 +36,8 @@ class vSphereUtil():
         self.vcenter_username = None
         self.vcenter_password = None
         self.vsphere_client = None
+
+        self.placement_spec = None
 
         self.session = requests.session()
         self.session.verify = False
@@ -58,15 +68,54 @@ class vSphereUtil():
     def list_vms(self):
         print(self.vsphere_client.vcenter.VM.list())
 
-    def create_default_vm(self):
+    def create_default_vm(self, vm_spec):
+        datacenter_name = vm_spec['vm_datacenter_name']
+        vm_folder_name = vm_spec['vm_folder_name']
+        datastore_name = vm_spec['vm_datastore_name']
 
-        return
+        if not self.placement_spec:
+            self.placement_spec = vm_placement_helper.get_placement_spec_for_resource_pool(
+                    self.vsphere_client,
+                    datacenter_name,
+                    vm_folder_name,
+                    datastore_name)
+
+        guest_os = vm_spec['vm_guestos']
+        vm_create_spec = VM.CreateSpec(
+                name=vm_spec['vm_name_default'],
+                guest_os=guest_os,
+                placement=self.placement_spec)
+
+        print('\n# Example: create_default_vm: Creating a VM using spec\n-----')
+        print(pp(vm_create_spec))
+        print('-----')
+
+        vm = self.vsphere_client.vcenter.VM.create(vm_create_spec)
+        print("create_default_vm: Created VM '{}' ({})".format(vm_spec['vm_name_default'], vm))
+
+        vm_info = self.vsphere_client.vcenter.VM.get(vm)
+        print('vm.get({}) -> {}'.format(vm, pp(vm_info)))
+        return vm
+
 
     def create_vm(self, vm_info):
         # TODO:
         print('Not implemented yet.', file=sys.stderr)
 
         return
+
+    def cleanup_default_vm(self, vm_spec):
+        vm = get_vm(self.vsphere_client, vm_spec['vm_name_default'])
+        if vm:
+            state = self.vsphere_client.vcenter.vm.Power.get(vm)
+            if state == Power.Info(state=Power.State.POWERED_ON):
+                self.vsphere_client.vcenter.vm.Power.stop(vm)
+            elif state == Power.Info(state=Power.State.SUSPENDED):
+                self.vsphere_client.vcenter.vm.Power.start(vm)
+                self.vsphere_client.vcenter.vm.Power.stop(vm)
+            print("Deleting VM '{}' ({})".format(self.vm_name, vm))
+            self.vsphere_client.vcenter.VM.delete(vm)
+
 
     def cleanup_vm(self, vm_info):
         # TODO:
