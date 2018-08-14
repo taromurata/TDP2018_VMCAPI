3156fdf4b68cf4b7d1b93a184da78470dd107abf
Taro Murata <taro.murata.1992@gmail.com>
2018-08-14 11:25:36 +0900

    Add zip-alexa-program.sh

---
+++ zip-alexa-program.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+#
+# zip-alexa-program.sh
+# Copyright (C) 2018 taro <taro@mba0.local>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+zip -r alexa-program.zip alexa-program
